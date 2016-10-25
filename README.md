881b103fe69165d0e9384988537562c714c8d408
Diego Ximenes <dxmendes1@gmail.com>
2016-10-25 02:57:52 -0200

    tmux plugins

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Requeriments  
-- tmux version: tmux 2.2  
+    - tmux version: tmux 2.2  
+    - xclip  
+
+tmux plugins installations: prefix + I  
--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ dir=$(dirname $full_path)/
 rm ~/.bashrc
 ln -s $dir/.bashrc ~/.bashrc
 rm ~/.tmux.conf
-ln -s $dir/.tmux.conf ~/.tmux.conf
+ln -s $dir/tmux/.tmux.conf ~/.tmux.conf

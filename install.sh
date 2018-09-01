a83ba4e907fcaf0b8584a56c8874195c72f8b605
Diego Ximenes Mendes <dxmendes1@gmail.com>
2018-09-01 17:56:26 -0300

    install.sh refactor

--- install.sh
+++ install.sh
@@ -3,30 +3,29 @@
 dir_file="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 dir_home=$(realpath ~)
 
-ln -snf $dir_file/terminator $dir_home/.config/terminator
+ln -snf "$dir_file/git/.gitconfig" "$dir_home/.gitconfig"
 
-ln -snf $dir_file/git/.gitconfig $dir_home/.gitconfig
+ln -snf "$dir_file/tmux/.tmux.conf" "$dir_home/.tmux.conf"
 
-ln -snf $dir_file/tmux/.tmux.conf $dir_home/.tmux.conf
+ln -snf "$dir_file/.inputrc" "$dir_home/.inputrc"
 
-ln -snf $dir_file/.inputrc $dir_home/.inputrc
+ln -snf "$dir_file/.toprc" "$dir_home/.toprc"
 
-ln -snf $dir_file/.toprc $dir_home/.toprc
-
-ln -snf $dir_file/.Xresources $dir_home/.Xresources
+ln -snf "$dir_file/.Xresources" "$dir_home/.Xresources"
 xrdb ~/.Xresources
 
-ln -snf $dir_file/rxvt-unicode-256color $dir_home/.urxvt
+ln -snf "$dir_file/terminator" "$dir_home/.config/terminator"
 
-ln -snf $dir_file/oh-my-zsh/.zshrc $dir_home/.zshrc
-ln -snf $dir_file/oh-my-zsh/my_gentoo.zsh-theme $dir_home/.oh-my-zsh/themes/my_gentoo.zsh-theme
+ln -snf "$dir_file/rxvt-unicode-256color" "$dir_home/.urxvt"
+
+ln -snf "$dir_file/oh-my-zsh/.zshrc" "$dir_home/.zshrc"
+ln -snf "$dir_file/oh-my-zsh/my_gentoo.zsh-theme" "$dir_home/.oh-my-zsh/themes/my_gentoo.zsh-theme"
 
 # set default shell to zsh
-sudo chsh -s $(which zsh)
+sudo chsh -s "$(which zsh)"
 
-de=$(echo $DESKTOP_SESSION)
-if [ "$de" == "plasma" ]; then
-    ln -snf $dir_file/kde/kglobalshortcutsrc $dir_home/.config/kglobalshortcutsrc
+if [ "$DESKTOP_SESSION" == "plasma" ]; then
+    ln -snf "$dir_file/kde/kglobalshortcutsrc" "$dir_home/.config/kglobalshortcutsrc"
     # TODO: find how to reload configs without logout/reboot
     sudo reboot
 fi

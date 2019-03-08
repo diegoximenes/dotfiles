fd6da1773f78777903f9a63c86429037b295f653
Diego Ximenes Mendes <dxmendes1@gmail.com>
2019-03-08 03:20:23 -0300

    better monitor switcher

--- home/.scripts/select_display.sh
+++ home/.scripts/select_display.sh
@@ -1,127 +1,174 @@
 #!/bin/bash
 
+disconnected="$(xrandr -q | grep 'disconnected' | awk '{print $1}')"
+connected="$(xrandr -q | grep ' connected' | awk '{print $1}')"
+
+################################################################################
+
+restart_dunst_i3() {
+    # restart dunst to ensure proper location on screen
+    pgrep -x dunst >/dev/null && killall dunst
+    setsid dunst 2>/dev/null &
+
+    # restart i3 to ensure proper location of polybar
+    i3-msg restart
+}
+
 get_resolution() {
     local screen
     screen="$1"
 
-    xrandr -q \
+    local res
+    res="$(xrandr -q \
         | sed -n "/^$screen/,/\+/p" \
         | tail -n 1 \
-        | awk '{print $1}'
-}
+        | awk '{print $1}')"
 
-get_x_resolution() {
-    local res
-    res="$1"
-    echo "$res" | sed 's/x.*//'
-}
+    local x_res
+    x_res="$(echo "$res" | sed 's/x.*//')"
+    local y_res
+    y_res="$(echo "$res" | sed 's/.*x//')"
 
-get_y_resolution() {
-    local res
-    res="$1"
-    echo "$res" | sed 's/.*x//'
+    echo "$x_res $y_res"
 }
 
 mirror_screen() {
     local primary
     primary="$1"
-    local secondary
-    secondary="$2"
-
-    local res_primary
-    res_primary="$(get_resolution "$primary")"
-
-    local res_secondary
-    res_secondary="$(get_resolution "$secondary")"
 
     local x_res_primary
-    x_res_primary="$(get_x_resolution "$res_primary")"
     local y_res_primary
-    y_res_primary="$(get_y_resolution "$res_primary")"
-    local x_res_secondary
-    x_res_secondary="$(get_x_resolution "$res_secondary")"
-    local y_res_secondary
-    y_res_secondary="$(get_y_resolution "$res_secondary")"
+    read -r x_res_primary y_res_primary < <(get_resolution "$primary")
 
-    local scale_x
-    scale_x=$(echo "$x_res_primary / $x_res_secondary" | bc -l)
-    local scale_y
-    scale_y=$(echo "$y_res_primary / $y_res_secondary" | bc -l)
+    local xrandr_cmd
+    xrandr_cmd="xrandr --output $primary --auto --scale 1.0x1.0"
+    for secondary in $connected; do
+        if [[ "$secondary" != "$primary" ]]; then
+            local x_res_secondary
+            local y_res_secondary
+            read -r x_res_secondary y_res_secondary < <(get_resolution "$secondary")
 
-    xrandr --output "$primary" --auto --scale 1.0x1.0 \
-        --output "$secondary" --auto --same-as "$primary" \
-        --scale "$scale_x"x"$scale_y"
+            local scale_x
+            scale_x=$(echo "$x_res_primary / $x_res_secondary" | bc -l)
+            local scale_y
+            scale_y=$(echo "$y_res_primary / $y_res_secondary" | bc -l)
+
+            xrandr_cmd="$xrandr_cmd --output $secondary --auto --same-as $primary --scale ${scale_x}x${scale_y}"
+        fi
+    done
+    eval "$xrandr_cmd"
 }
 
-function split_screen() {
-    direction="$(printf "left-of\nright-of\nabove\nbelow" | rofi -dmenu -i -p "which side of $primary should $secondary be on?")"
-    xrandr --output "$primary" --auto --scale 1.0x1.0 --output "$secondary" --"$direction" "$primary" --auto --scale 1.0x1.0
-}
-
-two_screens() {
-    local connected
-    connected="$1"
-
+split_screen() {
     local primary
-    primary=$(echo "$connected" | rofi -dmenu -i -p "which one is the primary?")
-    [[ "$primary" == "" ]] && exit
-    local secondary
-    secondary=$(echo "$connected" | grep -v "$primary")
-
-    local mirror
-    mirror=$(printf "yes\nno" | rofi -dmenu -i -p "mirror?")
-    case "$mirror" in
-        yes) mirror_screen "$primary" "$secondary" ;;
-        no) split_screen "$primary" "$secondary" ;;
-    esac
-}
-
-multi_screen() {
-    local screens
-    screens="$1"
-    local connected
-    connected="$2"
-
-    case "$(echo "$connected" | wc -l)" in
-        2) two_screens "$connected" ;;
-        *) notify-send -u normal 'Too much screens, configure it manually.' ;;
-    esac
+    primary="$1"
+    direction="$(printf "left-of\nright-of\nabove\nbelow" \
+        | rofi -dmenu -i -p "which side of $primary should $secondary be on?")"
+    xrandr --output "$primary" --auto --scale 1.0x1.0 \
+        --output "$secondary" --"$direction" "$primary" --auto --scale 1.0x1.0
 }
 
 select_screen() {
-    local screens
-    screens="$1"
     local selected
-    selected="$2"
+    selected="$1"
 
-    xrandr --output "$selected" --auto --scale 1.0x1.0 $(echo "$screens" | grep -v "$selected" | awk '{print "--output", $1, "--off"}' | tr '\n' ' ')
+    local xrandr_cmd
+    xrandr_cmd="xrandr --output $selected --auto --scale 1.0x1.0"
+    local screens
+    screens="$connected $disconnected"
+    for screen in $screens; do
+        if [[ "$screen" != "$selected" ]]; then
+            xrandr_cmd="$xrandr_cmd --output $screen --off"
+        fi
+    done
+    eval "$xrandr_cmd"
 }
 
-main() {
-    local screens
-    screens="$(xrandr -q | grep 'connected')"
+select_primary() {
+    echo "$connected" | rofi -dmenu -i -no-custom -p  "which one is the primary?"
+}
 
-    local connected
-    connected="$(echo "$screens" | grep ' connected' | awk '{print $1}')"
+exit_case_empty() {
+    local param
+    param="$1"
+    [[ "$param" == "" ]] && exit
+}
+
+mirror_config() {
+    local primary
+    primary="$(select_primary)"
+    exit_case_empty "$primary"
+    mirror_screen "$primary"
+}
+
+split_config() {
+    local cnt_connected
+    cnt_connected=$(echo "$connected" | wc -l)
+    if [[ "$cnt_connected" -gt '2' ]]; then
+        notify-send 'Unable to handle more than 2 screens, please configure it manually.' -u 'critical'
+    else
+        local primary
+        primary="$(select_primary)"
+        local secondary
+        secondary="$(echo "$connected" | grep -v -w "$primary")"
+        exit_case_empty "$primary"
+        split_screen "$primary" "$secondary"
+    fi
+
+}
+
+mirror_largest_screen() {
+    # get largest screen
+    local largest_screen
+    largest_screen=""
+    local largest_size
+    largest_size=0
+    for screen in $connected; do
+        local x_res
+        local y_res
+        read -r x_res y_res < <(get_resolution "$screen")
+
+        local size
+        size="$(("$x_res" * "$y_res"))"
+        if [[ "$size" -gt "$largest_size" ]]; then
+            largest_size=$size
+            largest_screen=$screen
+        fi
+    done
+
+    mirror_screen "$largest_screen"
+    restart_dunst_i3
+}
+
+user_config() {
     local cnt_connected
     cnt_connected=$(echo "$connected" | wc -l)
 
-    if (( cnt_connected == 1 )); then
-        select_screen "$screens" "$connected"
+    if [[ "$cnt_connected" -eq "1" ]]; then
+        select_screen "$connected"
     else
         local opt
-        opt="$(printf '%s\nmulti_screen\nmanual' "$connected" \
-            | rofi -dmenu -i -p 'which screen arrangement?')"
+        opt="$(printf '%s\ndefault\nmirror\nsplit' "$connected" \
+            | rofi -dmenu -i -no-custom -p 'which screen arrangement?')"
         case "$opt" in
-            'multi_screen') multi_screen "$screens" "$connected" ;;
-            'manual') arandr & ;;
-            *) select_screen "$screens" "$opt" ;;
+            'default') default_config ;;
+            'mirror') mirror_config ;;
+            'split') split_config ;;
+            '') exit ;;
+            *) select_screen "$opt" ;;
         esac
     fi
 
-    # restart dunst to ensure proper location on screen
-    pgrep -x dunst >/dev/null && killall dunst
-    setsid dunst 2>/dev/null &
+    restart_dunst_i3
 }
 
-main
+default_config() {
+    mirror_largest_screen
+}
+
+case "$1" in
+    --default_config)
+        default_config
+        ;;
+    *) user_config ;;
+esac

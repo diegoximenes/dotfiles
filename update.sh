b7b4219fd57bc11ff166e2c6cae3c8b3be6de902
Diego Ximenes <dxmendes1@gmail.com>
2020-09-17 23:13:43 -0300

    improve update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,65 @@
 #!/bin/bash
 
-yay -Syu
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
-# remove orphan packages
-sudo pacman -Rns "$(pacman -Qtdq)"
+set -e
+finish() {
+    if [[ ! "$?" -eq 0 ]]; then
+        echo -e "${RED}FAILED: ${BASH_COMMAND}${NC}"
+    fi
+}
+trap finish EXIT
 
-eval "$(ssh-agent)"
-ssh-add
+echo_step() {
+    local step
+    step="$1"
+    echo -e "${BLUE}$step${NC}"
+}
 
-nvim +PlugUpdate
-nvim +PlugClean
-nvim +CocUpdate
+success() {
+    echo -e "${GREEN}SUCCESS${NC}"
+}
+
+ssh_agent() {
+    eval "$(ssh-agent)"
+    ssh-add
+}
+
+update_submodules() {
+    echo_step 'Updating submodules...'
+    git submodule update --init --remote
+    git submodule foreach 'git submodule update --init --recursive'
+}
+
+update_arch_packages() {
+    echo_step 'Updating arch packages...'
+    yay -Syu
+}
+
+remove_orphan_arch_packages() {
+    echo_step 'Removing orphan arch packages...'
+    sudo pacman -Rns "$(pacman -Qtdq)"
+}
+
+update_nvim() {
+    echo_step 'Updating nvim stuff...'
+    nvim +PlugUpdate
+    nvim +PlugClean
+    nvim +CocUpdate
+}
+
+update_tmux_plugins() {
+    echo_step 'Updating tmux plugins...'
+    ~/.tmux/plugins/tpm/bin/update_plugins all
+}
+
+ssh_agent
+update_submodules
+update_arch_packages
+remove_orphan_arch_packages
+update_nvim
+update_tmux_plugins
+success

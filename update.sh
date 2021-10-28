433c04c0ac66fb17e034594dc305ada9593ee3e7
Diego Ximenes <dxmendes1@gmail.com>
2021-10-28 17:07:13 -0300

    update vim-plug procedure

--- update.sh
+++ update.sh
@@ -58,8 +58,15 @@ remove_orphan_arch_packages() {
     fi
 }
 
+update_vim_plug() {
+    curl -fLo ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
+
 update_nvim() {
     echo_step 'Updating nvim stuff...'
+
+    update_vim_plug
+
     nvim +PlugUpdate
     nvim +PlugClean
     nvim +CocUpdate

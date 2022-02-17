478f411bf6e3b55150ad05e9223543c02cc9eba6
Diego Ximenes <dxmendes1@gmail.com>
2022-02-17 00:33:25 -0300

    avoid low battery alert if there is no battery

--- home/.scripts/low_battery.sh
+++ home/.scripts/low_battery.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 CAPACITY_THRESHOLD=20
-[[ "$(cat /sys/class/power_supply/BAT0/status)" == "Charging" ]] && exit
-if [[ "$(cat /sys/class/power_supply/BAT0/capacity)" -lt $CAPACITY_THRESHOLD ]]; then
-    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus && notify-send -u critical "Battery too low."
+
+battery_dir="/sys/class/power_supply/BAT0/"
+if [[ -d "$battery_dir" ]]; then
+    [[ "$(cat "$battery_dir"/status)" == "Charging" ]] && exit
+    if [[ "$(cat "$battery_dir"/capacity)" -lt $CAPACITY_THRESHOLD ]]; then
+        export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus && notify-send -u critical "Battery too low."
+    fi
 fi

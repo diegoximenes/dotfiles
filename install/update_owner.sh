596d4fe6b64d2909fb474e37baf7b2ec82987a99
Diego Ximenes <dxmendes1@gmail.com>
2022-04-26 16:01:03 -0300

    update_owner.sh

---
+++ install/update_owner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+path_current_file="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+path_dotfiles="$(realpath "$path_current_file/..")"
+
+update_owner() {
+    local root_files
+    root_files=$(ls -I etc "$path_dotfiles")
+    for root_file in $root_files; do
+        sudo chown -R $USER "$root_file"
+    done
+}
+
+update_owner

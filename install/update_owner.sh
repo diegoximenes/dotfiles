fa5225bd13eb62f48439e16d6398be3c51cf4a64
Diego Ximenes <dxmendes1@gmail.com>
2022-04-27 14:09:05 -0300

    fix update_owner.sh

--- install/update_owner.sh
+++ install/update_owner.sh
@@ -5,9 +5,9 @@ path_dotfiles="$(realpath "$path_current_file/..")"
 
 update_owner() {
     local root_files
-    root_files=$(ls -I etc "$path_dotfiles")
+    root_files=$(ls -a -I etc -I . -I .. "$path_dotfiles")
     for root_file in $root_files; do
-        sudo chown -R $USER "$root_file"
+        sudo chown -R "$USER" "$path_dotfiles/$root_file"
     done
 }
 

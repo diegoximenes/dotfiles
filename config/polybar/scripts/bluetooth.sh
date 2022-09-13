1385ffaff4d8cc6a11ebadb7fee0d88eb6ab6310
Diego Ximenes <dxmendes1@gmail.com>
2022-09-13 17:32:16 -0300

    fix polybar bluetooth script

--- config/polybar/scripts/bluetooth.sh
+++ config/polybar/scripts/bluetooth.sh
@@ -20,6 +20,15 @@ device_connected() {
     fi
 }
 
+device_paired() {
+    device_info=$(bluetoothctl info "$1")
+    if echo "$device_info" | grep -q "Paired: yes"; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 power_toggle() {
     if power_on; then
         bluetoothctl power off
@@ -47,18 +56,20 @@ print_info() {
 
             local count_connected_devices=0
             local count_paired_devices=0
-            local paired_devices
-            mapfile -t paired_devices < <(bluetoothctl paired-devices | cut -d ' ' -f 2)
-            for i in "${!paired_devices[@]}"; do
-                count_paired_devices=$((count_paired_devices+1))
+            local devices
+            mapfile -t devices < <(bluetoothctl devices | cut -d ' ' -f 2)
+            for i in "${!devices[@]}"; do
+                if device_paired "${devices[$i]}"; then
+                    count_paired_devices=$((count_paired_devices+1))
+                fi
 
-                local device_alias
-                device_alias=$(bluetoothctl info "${paired_devices[$i]}" | \
-                    grep "Alias" | \
-                    cut -d ' ' -f 2-
-                )
+                if device_connected "${devices[$i]}"; then
+                    local device_alias
+                    device_alias=$(bluetoothctl info "${devices[$i]}" | \
+                        grep "Alias" | \
+                        cut -d ' ' -f 2-
+                    )
 
-                if device_connected "${paired_devices[$i]}"; then
                     local delimiter
                     delimiter=$(get_delimiter $count_connected_devices)
                     out="$out$delimiter${device_alias}"

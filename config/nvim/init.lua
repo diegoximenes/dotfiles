8914b47179e5aebecbe513cb10dd1e5977f015cf
Diego Ximenes <dxmendes1@gmail.com>
2024-05-19 21:04:46 -0300

    nvim: rm packer

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -196,15 +196,6 @@ local on_attach = function(_, bufnr)
 	end
 end
 
-local use = require("packer").use
-require("packer").startup(function()
-	use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
-	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
-	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
-	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
-	use("L3MON4D3/LuaSnip") -- Snippets plugin
-end)
-
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities["offsetEncoding"] = "utf-16"

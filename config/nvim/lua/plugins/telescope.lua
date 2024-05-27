d621438d8f5d35090457a05c4a67a3e5ef687153
Diego Ximenes <dxmendes1@gmail.com>
2024-05-26 23:34:46 -0300

    nvim: aerial

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,11 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim", "stevearc/aerial.nvim" },
 	config = function()
-		require("telescope").setup({
+		local telescope = require("telescope")
+
+		telescope.setup({
 			defaults = {
 				dynamic_preview_title = true,
 				layout_strategy = "vertical",
@@ -14,10 +16,14 @@ return {
 				},
 			},
 		})
+
+		telescope.load_extension("aerial")
+
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<C-i>", builtin.lsp_definitions, { noremap = true, silent = true })
 		vim.keymap.set("n", "<C-g>", builtin.lsp_type_definitions, { noremap = true, silent = true })
 		vim.keymap.set("n", "<C-n>", builtin.lsp_references, { noremap = true, silent = true })
 		vim.keymap.set("n", "<C-y>", builtin.lsp_implementations, { noremap = true, silent = true })
+		vim.keymap.set("n", "<C-s>", telescope.extensions.aerial.aerial, { noremap = true, silent = true })
 	end,
 }

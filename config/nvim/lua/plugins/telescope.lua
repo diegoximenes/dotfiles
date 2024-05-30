430e218b5e7c34256eb2eb802cf656b3077bb3fc
Diego Ximenes <dxmendes1@gmail.com>
2024-05-30 12:16:04 -0300

    nvim: telescope only shows diagnostics of current file

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -21,11 +21,18 @@ return {
 		telescope.load_extension("aerial")
 
 		local builtin = require("telescope.builtin")
+		local diagnostics = function()
+			-- only shows diagnostics for the current file
+			return builtin.diagnostics({
+				bufnr = 0,
+			})
+		end
+
 		vim.keymap.set("n", "<C-i>", builtin.lsp_definitions, { noremap = true, silent = true })
 		vim.keymap.set("n", "<C-g>", builtin.lsp_type_definitions, { noremap = true, silent = true })
 		vim.keymap.set("n", "<C-n>", builtin.lsp_references, { noremap = true, silent = true })
 		vim.keymap.set("n", "<C-y>", builtin.lsp_implementations, { noremap = true, silent = true })
 		vim.keymap.set("n", "<C-s>", telescope.extensions.aerial.aerial, { noremap = true, silent = true })
-		vim.keymap.set("n", "<C-q>", builtin.diagnostics, { noremap = true, silent = true })
+		vim.keymap.set("n", "<C-q>", diagnostics, { noremap = true, silent = true })
 	end,
 }

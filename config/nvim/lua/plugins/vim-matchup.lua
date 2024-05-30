285232873a468f1b245d4ebf80a17489b8fadaa2
Diego Ximenes <dxmendes1@gmail.com>
2024-05-30 12:05:49 -0300

    nvim: better mapping for vim-matchup

--- config/nvim/lua/plugins/vim-matchup.lua
+++ config/nvim/lua/plugins/vim-matchup.lua
@@ -2,5 +2,7 @@ return {
 	"andymass/vim-matchup",
 	config = function()
 		vim.g.matchup_matchparen_offscreen = {}
+		vim.api.nvim_set_keymap("n", "<Leader>m", "<plug>(matchup-]%)", { noremap = true })
+		vim.api.nvim_set_keymap("n", "<Leader>M", "<plug>(matchup-[%)", { noremap = true })
 	end,
 }

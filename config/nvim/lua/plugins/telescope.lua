b6c7be89eb925aeab61c5a92a565aae85e20feec
Diego Ximenes <dxmendes1@gmail.com>
2026-02-10 20:57:45 -0300

    Updates telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim", "stevearc/aerial.nvim" },
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	},
 	event = "LspAttach",
 	config = function()
 		local telescope = require("telescope")
